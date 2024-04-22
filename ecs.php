c7585248a8bd776f5adc1e218bcc601258932c5b
cezary.bilda <cezary.bilda@paylinksolutions.co.uk>
2024-04-22 07:55:52 +0100

    ecs

---
+++ ecs.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+
+    // add a single rule
+    ->withRules([
+        NoUnusedImportsFixer::class,
+    ])
+    ->withSets([
+        SetList::CLEAN_CODE,
+        SetList::PSR_12,
+    ])
+    ->withRules([
+        ArraySyntaxFixer::class,
+    ]);
+
+    // add sets - group of rules
+   // ->withPreparedSets(
+        // arrays: true,
+        // namespaces: true,
+        // spaces: true,
+        // docblocks: true,
+        // comments: true,
+    // )
+
+     ;
