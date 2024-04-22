71c7c0c0578d46e338b8b21a1d9e7cf145672865
cezary.bilda <cezary.bilda@paylinksolutions.co.uk>
2024-04-22 14:48:25 +0100

    scribe updated

--- app/Http/Controllers/Event/FindEventsByDateRangeController.php
+++ app/Http/Controllers/Event/FindEventsByDateRangeController.php
@@ -7,8 +7,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Event\FindEventsByDateRangeRequest;
 use Illuminate\Http\JsonResponse;
 use Knuckles\Scribe\Attributes\Authenticated;
+use Knuckles\Scribe\Attributes\QueryParam;
 
 #[Authenticated]
+#[QueryParam("startDate", "string", "The start date", required: true, example: '2024-04-22T13:43:11')]
+#[QueryParam("endDate", "string", "The start date", required: true, example: '2024-04-22T13:43:11')]
 class FindEventsByDateRangeController extends Controller
 {
     public function __construct(
